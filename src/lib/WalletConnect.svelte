7deda5a267839daf285e6a66cabef36340bb34a9
GrungeElFz <elf0114_@hotmail.com>
2023-06-11 21:23:01 -0500

    Debugged:   Fixed typo on `.getSinger()` --> `.getSigner()` (src/lib/WalletConnect.svelte)

--- src/lib/WalletConnect.svelte
+++ src/lib/WalletConnect.svelte
@@ -4,7 +4,7 @@
 	async function connectWallet() {
 		let provider = new ethers.BrowserProvider(window.ethereum, 'any');
 		await provider.send('eth_requestAccounts', []);
-		const signer = provider.getSinger();
+		const signer = provider.getSigner();
 		const account = await signer.getAddress();
 		const chainID = await signer.getChainId();
 	}
