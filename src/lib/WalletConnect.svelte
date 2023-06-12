5f3e81705b5710ea016e265279bee69b9b9249d4
GrungeElFz <elf0114_@hotmail.com>
2023-06-11 20:55:26 -0500

    Added:    `WalletConnect` component (src/lib/WalletConnect.svelte)

---
+++ src/lib/WalletConnect.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { ethers } from 'ethers';
+
+	async function connectWallet() {
+		let provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+		await provider.send('eth_requestAccounts', []);
+		const signer = provider.getSinger();
+		const account = await signer.getAddress();
+		const chainID = await signer.getChainId();
+	}
+</script>
+
+<button class="btn">Attach Wallet</button>
