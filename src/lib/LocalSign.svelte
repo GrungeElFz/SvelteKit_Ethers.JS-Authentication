0b5b492efe4f24adfbb0e6c28615c40afffc68d9
GrungeElFz <elf0114_@hotmail.com>
2023-06-13 21:29:25 -0500

    Added:    `LocalSign` component (src/lib/LocalSign.svelte)

---
+++ src/lib/LocalSign.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { ethers } from 'ethers';
+	import { fly } from 'svelte/transition';
+
+	export let web3Props: Web3Props;
+
+	$: nonce = Math.floor(Math.random() * 1000000);
+	$: signature = '';
+	$: address = '';
+	$: signed = false;
+
+	let valid: boolean | null = null;
+	let expectedAddress: string;
+
+	const notYourAddr = '0x0000000000000000000000000000000000000000';
+</script>
