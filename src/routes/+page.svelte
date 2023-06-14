a02cb7ba33b68d6b37ef972e6637d936dfce0dae
GrungeElFz <elf0114_@hotmail.com>
2023-06-13 21:53:45 -0500

    Modified:   Added `bind:web3Props` to `WalletConnect` component (src/routes/+page.svelte)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import WalletConnect from "$lib/WalletConnect.svelte";
+	import WalletConnect from '$lib/WalletConnect.svelte';
+
+	export let web3Props: Web3Props;
 </script>
 
-<WalletConnect />
+<WalletConnect bind:web3Props />
